4f6adc34445d19778e0d3a55cc3266c968ba4424
JosipRadnik <soljicjosip1987@gmail.com>
2021-03-14 21:37:18 +0100

    ExpensePlanner: Chart - commit #31

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -13,69 +13,69 @@ class TransactionList extends StatelessWidget {
     return Container(
       height: 300,
       child: transactions.isEmpty 
-        ? Column(
-          children: <Widget>[
-            Text(
-              'No transactions added yet!', 
-              style: Theme.of(context).textTheme.headline6,
+      ? Column(
+        children: <Widget>[
+          Text(
+            'No transactions added yet!', 
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 200,
+            child: Image.asset(
+              'assets/images/waiting.png', 
+              fit: BoxFit.cover,
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              height: 200,
-              child: Image.asset(
-                'assets/images/waiting.png', 
-                fit: BoxFit.cover,
-              ),
-            ),
-          ],
-        )
-        : ListView.builder(
-          itemBuilder: (ctx, index) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
+          ),
+        ],
+      )
+      : ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Theme.of(context).primaryColor,
+                      width: 2,
                     ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).primaryColor,
-                        width: 2,
-                      ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$ ${transactions[index].amount.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Theme.of(context).primaryColor,
                     ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$ ${transactions[index].amount.toStringAsFixed(2)}',
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    Text(
+                      DateFormat('dd.MM.yyy').format(transactions[index].date),
                       style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor,
+                        color: Colors.grey,
                       ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        transactions[index].title,
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      Text(
-                        DateFormat('dd.MM.yyy').format(transactions[index].date),
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          },
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
         itemCount: transactions.length,
       ),
     );

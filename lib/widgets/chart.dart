c62f426b6b654f8cc75032b5156b0777977134f8
JosipRadnik <soljicjosip1987@gmail.com>
2021-03-29 14:02:32 +0200

    ExpensePlanner - commit #45

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -31,7 +31,7 @@ class Chart extends StatelessWidget {
         'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum
       };
-    });
+    }).reversed.toList();
   }
 
   double get totalSpending {
@@ -56,9 +56,11 @@ class Chart extends StatelessWidget {
               child: ChartBar(
                 data['day'],
                 data['amount'],
-                totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending,
+                totalSpending == 0.0
+                    ? 0.0
+                    : (data['amount'] as double) / totalSpending,
               ),
-            ); 
+            );
             //Text('${data['day']}: ${data['amount']}');
           }).toList(),
         ),

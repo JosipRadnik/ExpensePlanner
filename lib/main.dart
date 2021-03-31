088cad46cf5a0a417d2422ff953ac3bcb429a5d2
JosipRadnik <soljicjosip1987@gmail.com>
2021-03-31 11:41:52 +0200

    ExpensePlanner: Dynamic Size - commit #54

--- lib/main.dart
+++ lib/main.dart
@@ -108,28 +108,37 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _deleteTransaction(String id) {
     setState(() {
-      _userTransactions.removeWhere((tx) => tx.id == id);  
+      _userTransactions.removeWhere((tx) => tx.id == id);
     });
   }
-    
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Personal Expenses'), actions: <Widget>[
+    final appBar = AppBar(
+      title: Text('Personal Expenses'),
+      actions: <Widget>[
         IconButton(
           icon: Icon(Icons.add),
           onPressed: () => _startAddNewTransaction(context),
         ),
-      ]),
+      ],
+    );
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(_recentTransactions),
-            TransactionList(
-              _userTransactions, 
-              _deleteTransaction,
+            Container(
+              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height) * 0.4,
+              child: Chart(_recentTransactions),
+            ),
+            Container(
+              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height) * 0.6,
+              child: TransactionList(
+                _userTransactions,
+                _deleteTransaction,
+              ),
             ),
           ],
         ),
